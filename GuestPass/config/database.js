62fbda7135fe15b8a1c98dbd5a91c9845e8ab7cd
Abdul Rehman Ujjan <theabdulrehmanujjan@gmail.com>
2026-03-26 14:37:36 +0500

    fix(db): remove hardcoded SSL settings for Neon connection

--- GuestPass/config/database.js
+++ GuestPass/config/database.js
@@ -4,9 +4,6 @@ const { Pool } = require('pg');
 // Create connection pool
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  },
   max: 20, // Maximum number of clients in the pool
   idleTimeoutMillis: 30000, // Close idle clients after 30 seconds
   connectionTimeoutMillis: 10000, // Return error after 10 seconds if connection not available
