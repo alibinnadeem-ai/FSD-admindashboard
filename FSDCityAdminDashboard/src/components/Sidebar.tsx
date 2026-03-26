660b5dad16370bd3ddabe8bc94f668771a074592
alibinnadeem-ai <alibinnadeem.ai@gmail.com>
2026-03-26 19:45:44 +0500

    Update Sidebar.tsx

--- FSDCityAdminDashboard/src/components/Sidebar.tsx
+++ FSDCityAdminDashboard/src/components/Sidebar.tsx
@@ -67,7 +67,7 @@ export default function Sidebar({ sidebarOpen, setSidebarOpen }: SidebarProps) {
       <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
         <div className="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6 pb-4">
           <div className="flex h-16 shrink-0 items-center">
-            <h1 className="text-xl font-bold text-gray-900">FSD City Admin</h1>
+            <h1 className="text-xl font-bold text-gray-900">Grand FSD Admin</h1>
           </div>
           <nav className="flex flex-1 flex-col">
             <ul className="space-y-1">
